382d85a1f1b4ba2bd5d9b439d9ebd48e86139826
drossoft <drossoft@outlook.com>
2023-01-08 18:24:05 +0100

    Agregar archivos de proyecto.

---
+++ FlexLayoutSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlexLayoutSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FlexLayoutSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FlexLayoutSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
